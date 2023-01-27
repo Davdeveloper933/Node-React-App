fc401206bf37a0f79126c34fe9d391d96bd9fa49
David Istomin <daveistomin@gmail.com>
2023-01-27 16:17:52 +0400

    aaa

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Handle GET requests to /api route
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
